d4e9cc896fd7256bf9cbdb0b33f5816edc71f22f
Guillermo Alvarado <g.galvarado55@outlook.com>
2022-11-25 00:10:46 -0600

    added a javasript file to keep the array list more tidy. also, changed the query settings again.

---
+++ javasript.js
@@ -0,0 +1,51 @@
+var arr1 = [
+    "Tanque", "Sanador", "Daño"
+],
+    arr2 = [
+        "DK Sangre",
+        "DH Venganza",
+        "Druida Guardian",
+        "Monje Cervecero",
+        "Paladin Proteccion",
+        "Guerrero Proteccion"
+    ],
+    arr3 = [
+        "DK Escarcha",
+        "Dk Profano",
+        "Druida Feral",
+        "Druida Balance",
+        "Cazador Punteria",
+        "Hunter Bestias",
+        "Hunter Supervivencia",
+        "Mago Escarcha",
+        "Mago Fuego",
+        "Mago Arcano",
+        "Monje Viajedo del Viento",
+        "Paladin Reprension",
+        "Sacerdote Sombra",
+        "Picaro Asesinato",
+        "Picaro Sutileza",
+        "Picaro Forajido",
+        "Chaman Elemental",
+        "Chaman Mejora",
+        "Brujo Demoniologia",
+        "Brujo Destruccion",
+        "Brujo Afliccion",
+        "Guerrero Furia",
+        "Guerrero Armas",
+        "Evocador Devastacion",
+    ],
+    arr4 = [
+        "Restoration Druid",
+        "Chaman Restauracion",
+        "Monje Tejedor de Niebla",
+        "Paladin Sagrado",
+        "Sacerdote Sagrado",
+        "Sacerdote Disciplina",
+        "Evocador Preservacion"
+
+    ];
+
+// function showRandomFrom(chosenArray) {
+//     document.getElementsByClassName("text")[0].innerHTML = chosenArray[Math.floor(Math.random() * chosenArray.length)];
+// }
